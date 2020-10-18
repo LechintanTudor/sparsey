3da331f5ef18d2ff5c9a2282f1d482a3a592518b
Tudor Lechintan <lechintantudor02@gmail.com>
2020-10-18 19:19:19 +0300

    Implement `World`

--- src/world.rs
+++ src/world.rs
@@ -1,47 +1,75 @@
-use crate::{Component, GenericStorage, Storage};
+use crate::{AtomicRefCell, Component, GenericStorage, Ref, RefMut, Storage};
 use std::{any::TypeId, collections::HashMap};
 
 type ComponentTypeId = TypeId;
 
-pub struct StorageWrapper {
-    storage: Box<dyn GenericStorage>,
-}
-
-impl StorageWrapper {
-    pub fn downcast_storage<T>(&self) -> Option<&Storage<T>>
-    where
-        T: Component,
-    {
-        self.storage.as_any().downcast_ref::<Storage<T>>()
-    }
-
-    pub fn downcast_storage_mut<T>(&mut self) -> Option<&mut Storage<T>>
-    where
-        T: Component,
-    {
-        self.storage.as_any_mut().downcast_mut::<Storage<T>>()
-    }
-}
-
 #[derive(Default)]
 pub struct World {
-    // storages: HashMap<ComponentTypeId, RefCell<StorageWrapper>>,
+    storages: HashMap<ComponentTypeId, AtomicRefCell<Box<dyn GenericStorage>>>,
 }
 
 impl World {
-    pub fn insert_storage<T>(&mut self)
+    pub fn borrow<T>(&self) -> Option<Ref<Storage<T>>>
     where
         T: Component,
     {
-        /*
-        self.storages.insert(
-            TypeId::of::<T>(),
-            RefCell::new(StorageWrapper {
-                storage: Box::new(Storage::<T>::default()),
-            }),
-        );
-        */
+        self.storages.get(&TypeId::of::<T>()).map(|s| {
+            s.borrow()
+                .map(|s| s.as_any().downcast_ref::<Storage<T>>().unwrap())
+        })
+    }
+
+    pub fn borrow_mut<T>(&self) -> Option<RefMut<Storage<T>>>
+    where
+        T: Component,
+    {
+        self.storages.get(&TypeId::of::<T>()).map(|s| {
+            s.borrow_mut()
+                .map(|s| s.as_any_mut().downcast_mut::<Storage<T>>().unwrap())
+        })
+    }
+
+    pub fn insert<T, S>(&mut self, storage: S)
+    where
+        T: Component,
+        S: Into<Box<Storage<T>>>,
+    {
+        self.storages
+            .insert(TypeId::of::<T>(), AtomicRefCell::new(storage.into()));
+    }
+
+    pub fn remove<T>(&mut self)
+    where
+        T: Component,
+    {
+        self.storages.remove(&TypeId::of::<T>());
     }
 }
 
-impl World {}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn borrow() {
+        let mut world = World::default();
+        world.insert(Storage::<u16>::default());
+        world.insert(Storage::<u32>::default());
+
+        let _u16_ref1 = world.borrow::<u16>().unwrap();
+        let _u16_ref2 = world.borrow::<u16>().unwrap();
+
+        let _u32_ref1 = world.borrow::<u32>().unwrap();
+        let _u32_ref2 = world.borrow::<u32>().unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn borrow_mut() {
+        let mut world = World::default();
+        world.insert(Storage::<u16>::default());
+
+        let _u16_ref1 = world.borrow_mut::<u16>().unwrap();
+        let _u16_ref2 = world.borrow_mut::<u16>().unwrap();
+    }
+}
