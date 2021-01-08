04229217f7cb8295e4ce6a029b65a519dc408498
Tudor Lechintan <lechintantudor02@gmail.com>
2021-01-08 19:26:03 +0200

    Add functions which use the ResourceTypeId, instead of type parameters, to Resources

--- src/registry/resources.rs
+++ src/registry/resources.rs
@@ -41,12 +41,26 @@ impl Resources {
     where
         T: 'static,
     {
-        self.values
-            .remove(&TypeId::of::<T>())
-            .map(|r| match r.into_inner().downcast::<T>() {
+        self.remove_by_id(TypeId::of::<T>())
+            .map(|r| match r.downcast::<T>() {
                 Ok(r) => r,
                 Err(_) => unsafe { unreachable_unchecked() },
             })
+    }
+
+    pub fn remove_by_id(&mut self, id: ResourceTypeId) -> Option<Box<dyn Any>> {
+        self.values.remove(&id).map(|r| r.into_inner())
+    }
+
+    pub fn contains<T>(&self) -> bool
+    where
+        T: 'static,
+    {
+        self.contains_id(TypeId::of::<T>())
+    }
+
+    pub fn contains_id(&self, id: ResourceTypeId) -> bool {
+        self.values.contains_key(&id)
     }
 
     pub fn borrow<T>(&self) -> Option<Res<T>>
@@ -76,15 +90,27 @@ impl Resources {
             }))
         })
     }
+
+    pub fn borrow_by_id(&self, id: ResourceTypeId) -> Option<Res<dyn Any>> {
+        self.values
+            .get(&id)
+            .map(|r| Res(AtomicRef::map(r.borrow(), |r| r.as_ref())))
+    }
+
+    pub fn borrow_mut_by_id(&self, id: ResourceTypeId) -> Option<ResMut<dyn Any>> {
+        self.values
+            .get(&id)
+            .map(|r| ResMut(AtomicRefMut::map(r.borrow_mut(), |r| r.as_mut())))
+    }
 }
 
 pub struct Res<'a, T>(AtomicRef<'a, T>)
 where
-    T: 'static;
+    T: 'static + ?Sized;
 
 impl<T> Deref for Res<'_, T>
 where
-    T: 'static,
+    T: 'static + ?Sized,
 {
     type Target = T;
 
@@ -95,11 +121,11 @@ where
 
 pub struct ResMut<'a, T>(AtomicRefMut<'a, T>)
 where
-    T: 'static;
+    T: 'static + ?Sized;
 
 impl<T> Deref for ResMut<'_, T>
 where
-    T: 'static,
+    T: 'static + ?Sized,
 {
     type Target = T;
 
@@ -110,7 +136,7 @@ where
 
 impl<T> DerefMut for ResMut<'_, T>
 where
-    T: 'static,
+    T: 'static + ?Sized,
 {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
