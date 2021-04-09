a52ba98e5bb2a84b992f165bf5879755d4b459dc
Tudor Lechintan <lechintantudor02@gmail.com>
2021-04-09 09:12:46 +0300

    Implement basic Query

--- src/query/filter.rs
+++ src/query/filter.rs
@@ -1,7 +1,7 @@
 use crate::data::{Component, Entity};
 use crate::world::{Comp, GroupMask};
 
-pub unsafe trait QueryFilterComponent {
+pub unsafe trait GroupFilter {
 	fn mask(&self) -> GroupMask;
 
 	fn includes_all(&self, entity: Entity) -> bool;
@@ -9,7 +9,7 @@ pub unsafe trait QueryFilterComponent {
 	fn excludes_all(&self, entity: Entity) -> bool;
 }
 
-unsafe impl QueryFilterComponent for () {
+unsafe impl GroupFilter for () {
 	fn mask(&self) -> GroupMask {
 		GroupMask::default()
 	}
@@ -23,7 +23,15 @@ unsafe impl QueryFilterComponent for () {
 	}
 }
 
-unsafe impl<'a, T> QueryFilterComponent for &'a Comp<'a, T>
+pub unsafe trait GroupFilterComponent {
+	fn mask(&self) -> GroupMask;
+
+	fn includes(&self, entity: Entity) -> bool;
+
+	fn excludes(&self, entity: Entity) -> bool;
+}
+
+unsafe impl<'a, T> GroupFilterComponent for &'a Comp<'a, T>
 where
 	T: Component,
 {
@@ -34,19 +42,43 @@ where
 		}
 	}
 
-	fn includes_all(&self, entity: Entity) -> bool {
+	fn includes(&self, entity: Entity) -> bool {
 		self.storage.contains(entity)
 	}
 
-	fn excludes_all(&self, entity: Entity) -> bool {
+	fn excludes(&self, entity: Entity) -> bool {
 		!self.storage.contains(entity)
 	}
 }
 
-pub unsafe trait QueryFilter {
-	fn mask(&self) -> GroupMask;
+macro_rules! impl_filter {
+	($(($comp:ident, $idx:tt)),+) => {
+		unsafe impl<$($comp),+> GroupFilter for ($($comp,)+)
+		where
+			$($comp: GroupFilterComponent,)+
+		{
+			fn mask(&self) -> GroupMask {
+				$(self.$idx.mask())|+
+			}
 
-	fn includes_all(&self, entity: Entity) -> bool;
+			fn includes_all(&self, entity: Entity) -> bool {
+				$(self.$idx.includes(entity))&&+
+			}
 
-	fn excludes_all(&self, entity: Entity) -> bool;
+			fn excludes_all(&self, entity: Entity) -> bool {
+				$(self.$idx.excludes(entity))&&+
+			}
+		}
+	};
+}
+
+#[rustfmt::skip]
+mod impls {
+	use super::*;
+
+	impl_filter!((A, 0));
+	impl_filter!((A, 0), (B, 1));
+	impl_filter!((A, 0), (B, 1), (C, 2));
+	impl_filter!((A, 0), (B, 1), (C, 2), (D, 3));
+	impl_filter!((A, 0), (B, 1), (C, 2), (D, 3), (E, 4));
 }
