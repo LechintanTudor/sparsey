0dc6640294e21fb35eafc66e62d46ec7955e7fb7
Tudor Lechintan <lechintantudor02@gmail.com>
2021-05-09 12:10:28 +0300

    More work on queries

--- src/query/component_filter.rs
+++ src/query/component_filter.rs
@@ -1,15 +1,97 @@
 use crate::components::{Component, Entity, SparseArrayView, Ticks};
 use crate::dispatcher::{Comp, CompMut};
+use crate::query::IterInfo;
 use crate::world::{GroupInfo, GroupMask, QueryGroupInfo};
 
-pub struct QueryInfo<'a> {
-	entities: &'a [Entity],
-	world_tick: Ticks,
-	last_system_tick: Ticks,
+pub trait ComponentFilterElement {
+	fn includes(&self, entity: Entity) -> bool;
 }
 
-pub trait ComponentFilterElement {
-	fn includes(&self) -> bool;
+impl<'a, T> ComponentFilterElement for &'a Comp<'a, T>
+where
+	T: Component,
+{
+	fn includes(&self, entity: Entity) -> bool {
+		self.storage.contains(entity)
+	}
+}
+
+impl<'a, T> ComponentFilterElement for &'a CompMut<'a, T>
+where
+	T: Component,
+{
+	fn includes(&self, entity: Entity) -> bool {
+		self.storage.contains(entity)
+	}
+}
+
+impl<'a> ComponentFilterElement for SparseArrayView<'a> {
+	fn includes(&self, entity: Entity) -> bool {
+		self.contains(entity)
+	}
+}
+
+pub trait BaseComponentFilterElement<'a>
+where
+	Self: ComponentFilterElement,
+{
+	type Split: ComponentFilterElement;
+
+	fn world_tick(&self) -> Ticks;
+
+	fn last_system_tick(&self) -> Ticks;
+
+	fn group_info(&self) -> Option<GroupInfo>;
+
+	fn split(self) -> (&'a [Entity], Self::Split);
+}
+
+impl<'a, T> BaseComponentFilterElement<'a> for &'a Comp<'a, T>
+where
+	T: Component,
+{
+	type Split = SparseArrayView<'a>;
+
+	fn world_tick(&self) -> Ticks {
+		self.world_tick
+	}
+
+	fn last_system_tick(&self) -> Ticks {
+		self.last_system_tick
+	}
+
+	fn group_info(&self) -> Option<GroupInfo> {
+		self.group_info
+	}
+
+	fn split(self) -> (&'a [Entity], Self::Split) {
+		let (sparse, entities, _, _) = self.storage.split();
+		(entities, sparse)
+	}
+}
+
+impl<'a, T> BaseComponentFilterElement<'a> for &'a CompMut<'a, T>
+where
+	T: Component,
+{
+	type Split = SparseArrayView<'a>;
+
+	fn world_tick(&self) -> Ticks {
+		self.world_tick
+	}
+
+	fn last_system_tick(&self) -> Ticks {
+		self.last_system_tick
+	}
+
+	fn group_info(&self) -> Option<GroupInfo> {
+		self.group_info
+	}
+
+	fn split(self) -> (&'a [Entity], Self::Split) {
+		let (sparse, entities, _, _) = self.storage.split();
+		(entities, sparse)
+	}
 }
 
 pub trait ComponentFilter {
@@ -18,22 +100,13 @@ pub trait ComponentFilter {
 	fn excludes_all(&self, entity: Entity) -> bool;
 }
 
-pub trait ComponentFilterQuery<'a>
+pub trait BaseComponentFilter<'a>
 where
 	Self: ComponentFilter,
 {
 	type Split: ComponentFilter;
 
-	fn split(self) -> Option<(QueryInfo<'a>, Self::Split)>;
+	fn group_info(&self) -> Option<QueryGroupInfo>;
 
-	fn group_info(&self) -> Option<QueryGroupInfo<'a>>;
-}
-
-pub trait ComponentFilterQueryElement<'a>
-where
-	Self: ComponentFilterElement,
-{
-	fn split(self) -> (SparseArrayView<'a>, &'a [Entity]);
-
-	fn group_info(&self) -> GroupInfo;
+	fn split(self) -> (Option<IterInfo<'a>>, Self::Split);
 }
--- src/query/mod.rs
+++ src/query/mod.rs
@@ -1,16 +1,18 @@
 pub use self::component_filter::*;
 pub use self::element::*;
 pub use self::info_filter::*;
+pub use self::iter::*;
 
 mod component_filter;
 mod element;
 mod info_filter;
+mod iter;
 
 use crate::components::{Entity, Ticks};
 use crate::world::{CombinedQueryGroupInfo, QueryGroupInfo};
 
 #[derive(Debug)]
-pub struct IterData<'a> {
+pub struct IterInfo<'a> {
 	entities: &'a [Entity],
 	world_tick: Ticks,
 	last_system_tick: Ticks,
@@ -18,42 +20,83 @@ pub struct IterData<'a> {
 
 pub unsafe trait Query<'a> {
 	type Item;
+	type SparseSplit;
+	type DenseSplit;
+	type Include: BaseComponentFilter<'a>;
+	type Exclude: BaseComponentFilter<'a>;
+	type Filter: InfoFilter;
 
 	fn get(self, entity: Entity) -> Option<Self::Item>;
 
-	fn contains(&self, entity: Entity) -> bool;
+	fn includes(&self, entity: Entity) -> bool;
 
 	fn group_info(&self) -> Option<CombinedQueryGroupInfo>;
+
+	fn split_sparse(
+		self,
+	) -> (
+		IterInfo<'a>,
+		Self::SparseSplit,
+		<Self::Include as BaseComponentFilter<'a>>::Split,
+		<Self::Exclude as BaseComponentFilter<'a>>::Split,
+		Self::Filter,
+	);
+
+	fn split_dense(self) -> (IterInfo<'a>, Self::DenseSplit, Self::Filter);
+
+	unsafe fn get_from_sparse_split(
+		sparse: &mut Self::SparseSplit,
+		include: &<Self::Include as BaseComponentFilter<'a>>::Split,
+		exclude: &<Self::Exclude as BaseComponentFilter<'a>>::Split,
+		filter: &Self::Filter,
+		entity: Entity,
+		world_tick: Ticks,
+		last_system_tick: Ticks,
+	);
+
+	unsafe fn get_from_dense_split(
+		dense: &mut Self::DenseSplit,
+		filter: &Self::Filter,
+		entity: Entity,
+		world_tick: Ticks,
+		last_system_tick: Ticks,
+	);
 }
 
-macro_rules! impl_query {
-	($(($elem:ident, $idx:tt)),*) => {
-		unsafe impl<'a, $($elem),*> Query<'a> for ($($elem,)*)
-		where
-			$($elem: QueryElement<'a>,)*
-		{
-			type Item = ($($elem::Item,)*);
-
-			#[allow(unused_variables)]
-			fn get(self, entity: Entity) -> Option<Self::Item> {
-				Some(($(self.$idx.get(entity)?,)*))
-			}
-
-			#[allow(unused_variables)]
-			fn contains(&self, entity: Entity) -> bool {
-				true $(&& self.$idx.contains(entity))*
-			}
-
-			fn group_info(&self) -> Option<CombinedQueryGroupInfo> {
-				let info = QueryGroupInfo::new() $(.with_group(self.$idx.group_info()?)?)*;
-				CombinedQueryGroupInfo::new().include(info)
-			}
-		}
-	};
+pub trait SimpleQuery<'a>
+where
+	Self: Query<'a, Include = (), Exclude = ()>,
+{
 }
 
-impl_query!();
-impl_query!((A, 0));
-impl_query!((A, 0), (B, 1));
-impl_query!((A, 0), (B, 1), (C, 2));
-impl_query!((A, 0), (B, 1), (C, 2), (D, 3));
+// macro_rules! impl_query {
+// 	($(($elem:ident, $idx:tt)),*) => {
+// 		unsafe impl<'a, $($elem),*> Query<'a> for ($($elem,)*)
+// 		where
+// 			$($elem: QueryElement<'a>,)*
+// 		{
+// 			type Item = ($($elem::Item,)*);
+
+// 			#[allow(unused_variables)]
+// 			fn get(self, entity: Entity) -> Option<Self::Item> {
+// 				Some(($(self.$idx.get(entity)?,)*))
+// 			}
+
+// 			#[allow(unused_variables)]
+// 			fn contains(&self, entity: Entity) -> bool {
+// 				true $(&& self.$idx.contains(entity))*
+// 			}
+
+// 			fn group_info(&self) -> Option<CombinedQueryGroupInfo> {
+// 				let info = QueryGroupInfo::new() $(.with_group(self.$idx.group_info()?)?)*;
+// 				CombinedQueryGroupInfo::new().include(info)
+// 			}
+// 		}
+// 	};
+// }
+
+// impl_query!();
+// impl_query!((A, 0));
+// impl_query!((A, 0), (B, 1));
+// impl_query!((A, 0), (B, 1), (C, 2));
+// impl_query!((A, 0), (B, 1), (C, 2), (D, 3));
