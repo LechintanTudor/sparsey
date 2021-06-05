df3274fa769d4a808595fea33195aea310ba551a
Tudor Lechintan <lechintantudor02@gmail.com>
2021-06-05 13:04:00 +0300

    Rename world modules to be more descriptive

--- src/world/mod.rs
+++ src/world/mod.rs
@@ -1,22 +1,22 @@
 pub use self::component_set::*;
-pub use self::component_view::*;
-pub use self::components::*;
 pub use self::group_info::*;
 pub use self::group_mask::*;
+pub use self::storage_view::*;
+pub use self::storages::*;
 pub use self::used_group_families::*;
 pub use self::world::*;
 
-pub(crate) use self::entities::*;
-pub(crate) use self::grouped::*;
-pub(crate) use self::ungrouped::*;
+pub(crate) use self::entity_storage::*;
+pub(crate) use self::grouped_storages::*;
+pub(crate) use self::ungrouped_storages::*;
 
 mod component_set;
-mod component_view;
-mod components;
-mod entities;
+mod entity_storage;
 mod group_info;
 mod group_mask;
-mod grouped;
-mod ungrouped;
+mod grouped_storages;
+mod storage_view;
+mod storages;
+mod ungrouped_storages;
 mod used_group_families;
 mod world;
