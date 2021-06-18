bfa981fad5fe286a95421b18cc43b277a5c98e29
Tudor Lechintan <lechintantudor02@gmail.com>
2021-06-18 14:38:53 +0300

    Document Entity, IndexEntity and Version

--- src/components/entity.rs
+++ src/components/entity.rs
@@ -1,5 +1,6 @@
 use std::num::NonZeroU32;
 
+/// Handle used to fetch components from `ComponentStorages`.
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct Entity {
 	index: u32,
@@ -7,10 +8,12 @@ pub struct Entity {
 }
 
 impl Entity {
+	/// Creates a new entity with the given `index` and `version`.
 	pub const fn new(index: u32, version: Version) -> Self {
 		Self { index, version }
 	}
 
+	/// Creates a new entity with the given `index` and default version.
 	pub const fn with_index(index: u32) -> Self {
 		unsafe {
 			Self {
@@ -20,20 +23,24 @@ impl Entity {
 		}
 	}
 
+	/// Returns the `index` of the entity, extended to a usize.
 	pub const fn index(&self) -> usize {
 		self.index as _
 	}
 
+	/// Returns the `version` of the entity.
 	pub const fn version(&self) -> Version {
 		self.version
 	}
 
+	/// Creates an entity with the same index as the current one and next
+	/// version if the version is not the last possible one.
 	pub const fn with_next_version(&self) -> Option<Self> {
-		if self.version.get() < u32::MAX {
+		if self.version.id() < u32::MAX {
 			unsafe {
 				Some(Self::new(
 					self.index,
-					Version::new_unchecked(self.version.get() + 1),
+					Version::new_unchecked(self.version.id() + 1),
 				))
 			}
 		} else {
@@ -42,6 +49,7 @@ impl Entity {
 	}
 }
 
+/// Used internally by `SparseArray` to map `Entity` indexes to dense indexes.
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct IndexEntity {
 	index: u32,
@@ -49,10 +57,12 @@ pub struct IndexEntity {
 }
 
 impl IndexEntity {
+	/// Creates a new index entity with the given `index` and `version`.
 	pub const fn new(index: u32, version: Version) -> Self {
 		Self { index, version }
 	}
 
+	/// Creates a new index entity with the given `index` and default version.
 	pub const fn with_index(index: u32) -> Self {
 		unsafe {
 			Self {
@@ -62,20 +72,24 @@ impl IndexEntity {
 		}
 	}
 
+	/// Returns the `index` of the index entity, extended to a usize.
 	pub const fn index(&self) -> usize {
 		self.index as _
 	}
 
+	/// Returns the `version` of the index entity.
 	pub const fn version(&self) -> Version {
 		self.version
 	}
 
+	/// Creates an index entity with the same index as the current one and next
+	/// version if the version is not the last possible one.
 	pub const fn with_next_version(&self) -> Option<Self> {
-		if self.version.get() < u32::MAX {
+		if self.version.id() < u32::MAX {
 			unsafe {
 				Some(Self::new(
 					self.index,
-					Version::new_unchecked(self.version.get() + 1),
+					Version::new_unchecked(self.version.id() + 1),
 				))
 			}
 		} else {
@@ -84,6 +98,8 @@ impl IndexEntity {
 	}
 }
 
+/// Used by `Entities` to recycle indexes. `Entities` with the same index and
+/// different `Versions` are considered different.
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct Version(NonZeroU32);
 
@@ -94,15 +110,18 @@ impl Default for Version {
 }
 
 impl Version {
+	/// Creates a new version with the given `id`.
 	pub fn new(id: u32) -> Self {
 		Self(NonZeroU32::new(id).expect("Version ID cannot be zero"))
 	}
 
+	/// Creates a new version with the given `id`. The `id` must be `non-zero`.
 	pub const unsafe fn new_unchecked(id: u32) -> Self {
 		Self(NonZeroU32::new_unchecked(id))
 	}
 
-	pub const fn get(&self) -> u32 {
+	/// Returns the `id` of the version.
+	pub const fn id(&self) -> u32 {
 		self.0.get()
 	}
 }
