79d329c345e05df3f425022f3b3546543d7655d9
Tudor Lechintan <lechintantudor02@gmail.com>
2021-06-20 12:01:13 +0300

    Document ComponentTicks

--- src/components/ticks.rs
+++ src/components/ticks.rs
@@ -1,5 +1,6 @@
 pub type Ticks = u32;
 
+/// Contains the ticks in which a component was added and last mutated.
 #[derive(Copy, Clone, Eq, PartialEq, Default, Debug)]
 pub struct ComponentTicks {
 	tick_added: Ticks,
@@ -18,10 +19,12 @@ impl ComponentTicks {
 		self.tick_mutated = tick_mutated;
 	}
 
+	/// Returns the tick in which the component was added to the `World`.
 	pub fn tick_added(&self) -> Ticks {
 		self.tick_added
 	}
 
+	/// Returns the tick in which the component was last mutated.
 	pub fn tick_mutated(&self) -> Ticks {
 		self.tick_mutated
 	}
