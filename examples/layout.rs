07f94a22f02474e0766e3dc8c38e8be2cbc8341f
Tudor Lechintan <lechintantudor02@gmail.com>
2021-07-15 20:52:41 +0300

    Add layout example

---
+++ examples/layout.rs
@@ -0,0 +1,53 @@
+use sparsey::prelude::*;
+
+#[derive(Clone, Copy, Debug)]
+pub struct Position(i32, i32);
+
+#[derive(Clone, Copy, Debug)]
+pub struct Sprite {
+	id: u32,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Transparent;
+
+fn print_sprites(pos: Comp<Position>, sprites: Comp<Sprite>, transparencies: Comp<Transparent>) {
+	let (pos_slice, sprite_slice) = (&pos, &sprites).slices();
+	println!("[All sprites]");
+	println!("Positions: {:?}", pos_slice);
+	println!("Sprites: {:?}", sprite_slice);
+
+	let (pos_slice, sprite_slice) = (&pos, &sprites).include(&transparencies).slices();
+	println!("\n[Transparent sprites]");
+	println!("Positions: {:?}", pos_slice);
+	println!("Sprites: {:?}", sprite_slice);
+
+	let (pos_slice, sprite_slice) = (&pos, &sprites).exclude(&transparencies).slices();
+	println!("\n[Opaque sprites]");
+	println!("Positions: {:?}", pos_slice);
+	println!("Sprites: {:?}", sprite_slice);
+}
+
+fn main() {
+	let mut dispatcher = Dispatcher::builder()
+		.add_system(print_sprites.system())
+		.build();
+
+	let layout = Layout::builder()
+		.add_group(<(Position, Sprite)>::group())
+		.add_group(<(Position, Sprite, Transparent)>::group())
+		.build();
+
+	let mut world = World::with_layout(&layout);
+	dispatcher.set_up(&mut world);
+
+	world.create((Position(0, 0), Sprite { id: 0 }));
+	world.create((Position(1, 1), Sprite { id: 1 }));
+	world.create((Position(2, 2), Sprite { id: 2 }, Transparent));
+	world.create((Position(3, 3), Sprite { id: 3 }, Transparent));
+
+	let mut resources = Resources::default();
+
+	dispatcher.run_seq(&mut world, &mut resources).unwrap();
+	world.advance_ticks().unwrap();
+}
