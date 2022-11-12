a31dd268e82d9aa54d311926ffda1630a0cc9adb
Tudor Lechintan <lechintantudor02@gmail.com>
2022-11-12 14:58:57 +0200

    Update most tests

---
+++ tests/components_register.rs
@@ -0,0 +1,32 @@
+mod common;
+
+use self::common::*;
+use sparsey::prelude::*;
+
+#[test]
+fn test_components_register() {
+    let mut world = World::default();
+
+    // No components are registered at creation
+    assert!(!world.is_registered::<A>());
+    assert!(!world.is_registered::<B>());
+
+    // Register component A
+    world.register::<A>();
+    assert!(world.is_registered::<A>());
+
+    // Register component B
+    world.register::<B>();
+    assert!(world.is_registered::<A>());
+    assert!(world.is_registered::<B>());
+
+    // Components remain registered even after clear
+    world.clear();
+    assert!(world.is_registered::<A>());
+    assert!(world.is_registered::<B>());
+
+    // Components remain registered even after reset
+    world.reset();
+    assert!(world.is_registered::<A>());
+    assert!(world.is_registered::<B>());
+}
