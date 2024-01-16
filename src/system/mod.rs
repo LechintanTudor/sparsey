be8487fc73d08997493f492531c1bcbce326315d
Tudor Lechințan <lechintantudor02@gmail.com>
2024-01-16 17:06:35 +0200

    feat: make systems generic over their return type

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -10,19 +10,19 @@ use crate::entity::EntityStorage;
 use crate::resource::ResourceStorage;
 use crate::World;
 
-pub struct System<TRegistry = World> {
-    system_fn: Box<dyn FnMut(&TRegistry) + Send + Sync + 'static>,
+pub struct System<TRegistry = World, TReturn = ()> {
+    system_fn: Box<dyn FnMut(&TRegistry) -> TReturn + Send + Sync + 'static>,
     params: &'static [SystemParamKind],
 }
 
-impl<TRegistry> System<TRegistry> {
+impl<TRegistry, TReturn> System<TRegistry, TReturn> {
     #[must_use]
-    pub fn new<TParams>(f: impl IntoSystem<TRegistry, TParams>) -> Self {
+    pub fn new<TParams>(f: impl IntoSystem<TRegistry, TParams, TReturn>) -> Self {
         f.system()
     }
 
-    pub fn run(&mut self, registry: &TRegistry) {
-        (self.system_fn)(registry);
+    pub fn run(&mut self, registry: &TRegistry) -> TReturn {
+        (self.system_fn)(registry)
     }
 
     #[must_use]
@@ -31,9 +31,9 @@ impl<TRegistry> System<TRegistry> {
     }
 }
 
-pub trait IntoSystem<TRegistry, TParams> {
+pub trait IntoSystem<TRegistry, TParams, TReturn> {
     #[must_use]
-    fn system(self) -> System<TRegistry>;
+    fn system(self) -> System<TRegistry, TReturn>;
 }
 
 macro_rules! impl_into_system {
@@ -46,15 +46,15 @@ macro_rules! impl_into_system {
 
 macro_rules! impl_into_system_in {
     ($registry:ident: $Registry:ident; $($Param:ident),*) => {
-        impl<TFunc, $($Param),*> IntoSystem<$Registry, ($($Param,)*)> for TFunc
+        impl<TFunc, TReturn, $($Param),*> IntoSystem<$Registry, ($($Param,)*), TReturn> for TFunc
         where
-            TFunc: Run<$Registry, ($($Param,)*), ()> + Send + Sync + 'static,
-            for<'a> &'a mut TFunc: Run<$Registry, ($($Param,)*), ()>,
+            TFunc: Run<$Registry, ($($Param,)*), TReturn> + Send + Sync + 'static,
+            for<'a> &'a mut TFunc: Run<$Registry, ($($Param,)*), TReturn>,
         {
-            fn system(mut self) -> System<$Registry> {
+            fn system(mut self) -> System<$Registry, TReturn> {
                 System {
                     system_fn: Box::new(move |$registry: &$Registry| {
-                        Run::run(&mut self, $registry);
+                        Run::run(&mut self, $registry)
                     }),
                     params: TFunc::PARAMS,
                 }
