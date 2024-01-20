bb06c0a589c3aa70a50f34b404dfbe156676f27f
Tudor Lechințan <lechintantudor02@gmail.com>
2024-01-20 12:31:45 +0200

    docs: port old examples

---
+++ examples/components.rs
@@ -0,0 +1,53 @@
+use sparsey::prelude::*;
+
+#[derive(Clone, Copy, Debug)]
+struct Position(i32, i32);
+
+#[derive(Clone, Copy, Debug)]
+struct Velocity(i32, i32);
+
+#[derive(Clone, Copy, Debug)]
+struct Frozen;
+
+fn update_velocities(mut velocities: CompMut<Velocity>, frozens: Comp<Frozen>) {
+    println!("[Update velocities]");
+
+    (&mut velocities)
+        .include(&frozens)
+        .for_each_with_entity(|(entity, velocity)| {
+            println!("{:?} is frozen. Set its velocity to (0, 0)", entity);
+
+            *velocity = Velocity(0, 0);
+        });
+
+    println!();
+}
+
+fn update_positions(mut positions: CompMut<Position>, velocities: Comp<Velocity>) {
+    println!("[Update positions]");
+
+    (&mut positions, &velocities).for_each_with_entity(|(entities, (position, velocity))| {
+        position.0 += velocity.0;
+        position.1 += velocity.1;
+
+        println!("{:?}, {:?}, {:?}", entities, *position, velocity);
+    });
+
+    println!();
+}
+
+fn main() {
+    let mut entities = EntityStorage::default();
+    entities.register::<Position>();
+    entities.register::<Velocity>();
+    entities.register::<Frozen>();
+
+    entities.create((Position(0, 0), Velocity(1, 1)));
+    entities.create((Position(0, 0), Velocity(2, 2)));
+    entities.create((Position(0, 0), Velocity(3, 3), Frozen));
+
+    for _ in 0..3 {
+        entities.run(update_velocities);
+        entities.run(update_positions);
+    }
+}
---
+++ examples/layout.rs
@@ -0,0 +1,60 @@
+use sparsey::prelude::*;
+
+#[derive(Clone, Copy, Debug)]
+struct Position(i32, i32);
+
+#[derive(Clone, Copy, Debug)]
+struct Sprite {
+    #[allow(dead_code)]
+    id: u32,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Transparent;
+
+fn print_sprites(
+    positions: Comp<Position>,
+    sprites: Comp<Sprite>,
+    transparencies: Comp<Transparent>,
+) {
+    let (position_slice, sprite_slice) = (&positions, &sprites)
+        .group_components()
+        .expect("Not a group");
+
+    println!("[All sprites]");
+    println!("Positions: {:?}", position_slice);
+    println!("  Sprites: {:?}", sprite_slice);
+
+    let (position_slice, sprite_slice) = (&positions, &sprites)
+        .include(&transparencies)
+        .group_components()
+        .expect("Not a group");
+
+    println!("\n[Transparent sprites]");
+    println!("Positions: {:?}", position_slice);
+    println!("  Sprites: {:?}", sprite_slice);
+
+    let (position_slice, sprite_slice) = (&positions, &sprites)
+        .exclude(&transparencies)
+        .group_components()
+        .expect("Not a group");
+
+    println!("\n[Opaque sprites]");
+    println!("Positions: {:?}", position_slice);
+    println!("  Sprites: {:?}", sprite_slice);
+}
+
+fn main() {
+    let layout = GroupLayout::builder()
+        .add_group::<(Position, Sprite)>()
+        .add_group::<(Position, Sprite, Transparent)>()
+        .build();
+
+    let mut entities = EntityStorage::new(&layout);
+    entities.create((Position(0, 0), Sprite { id: 0 }));
+    entities.create((Position(1, 1), Sprite { id: 1 }));
+    entities.create((Position(2, 2), Sprite { id: 2 }, Transparent));
+    entities.create((Position(3, 3), Sprite { id: 3 }, Transparent));
+
+    entities.run(print_sprites);
+}
