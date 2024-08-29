c2389e1e80b77e576a991a68c78a1b88ac1b0a40
Tudor Lechințan <lechintantudor02@gmail.com>
2024-08-29 15:35:42 +0300

    docs: document root module

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,42 @@
-#![no_std]
+//! Sparsey is an [Entity Component System (ECS)](https://www.geeksforgeeks.org/sparse-set/)
+//! based on [sparse sets](https://www.geeksforgeeks.org/sparse-set/).
+//!
+//! # Example
+//! ```rust
+//! use sparsey::World;
+//!
+//! struct Position(i32, i32);
+//! struct Velocity(i32, i32);
+//!
+//! fn main() {
+//!     let mut world = World::builder()
+//!         .register::<Position>()
+//!         .register::<Velocity>()
+//!         .build();
+//!
+//!     world.create((Position(0, 0), Velocity(1, 2)));
+//!     world.create((Position(0, 0), Velocity(2, 3)));
+//!     
+//!     world.for_each::<(&mut Position, &Velocity)>(|(position, velocity)| {
+//!         position.0 += velocity.0;
+//!         position.1 += velocity.1;
+//!     });
+//! }
+//! ```
+//!
+//! # Features
+//!
+//! - `std` (on by default): link to the `std` crate.
+//! - `parallel`: enable parallel iterators.
+//!
+//! # Usage
+//!
+//! The most important items from Sparsey crate are [`World`] and [`Entity`], which are re-exported
+//! at the root of the crate for easy access. A [`World`] is a collection of entities and components
+//! thas supports create/read/update/delete (CRUD) operations, while an [`Entity`] is a versioned
+//! index used to reference components within a [`World`].
+
+#![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
 
